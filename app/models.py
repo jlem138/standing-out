50a26a79a2086ba74c65e7278a09cc5716c7cb3b
Joshua Lemkin <joshua.lemkin@gmail.com>
2018-11-27 01:51:03 -0500

    corrected relationships for foreign keys

--- app/models.py
+++ app/models.py
@@ -6,6 +6,30 @@ from sqlalchemy.orm import relationship
 
 from app import db, login_manager
 
+class League(db.Model):
+    """
+    Create an leagues table
+    """
+
+    __tablename__ = 'leagues'
+
+    league_name = db.Column(db.String(200), primary_key=True)
+    number_of_games = db.Column(db.Integer)
+    number_of_conferences = db.Column(db.Integer)
+    number_of_total_teams = db.Column(db.Integer)
+    number_of_rounds = db.Column(db.Integer)
+    number_of_qualifiers = db.Column(db.Integer)
+    is_byes = db.Column(db.String(200))
+
+    # one league to many teams, events, and updates
+    teams = db.relationship('Team', backref='team_league', lazy=True)
+    events = db.relationship('Event', backref='event_league', lazy=True)
+    updates = db.relationship('Update', backref='update_league', lazy=True)
+
+    def __repr__(self):
+        return '<League: {}>'.format(self.league_name)
+
+
 class User(UserMixin, db.Model):
     """
     Create an User table
@@ -22,6 +46,10 @@ class User(UserMixin, db.Model):
     last_name = db.Column(db.String(60), index=True)
     phone_number = db.Column(db.String(10), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+
+    # one user to many updates
+    updates = db.relationship('Update', backref='update_user', lazy=True)
+
 
     @property
     def password(self):
@@ -62,11 +90,15 @@ class Team(db.Model):
     division_name = db.Column(db.String(60))
     conference_name = db.Column(db.String(60))
     league_name = db.Column(db.String(60), db.ForeignKey('leagues.league_name'), nullable=False)
-    league_constraint = relationship("League", foreign_keys=[league_name])
+    #league_constraint = relationship("League", foreign_keys=[league_name])
     #event_winner = relationship("Event", cascade="save-update", foreign_keys=[winner])
     #event_loser = relationship("Event", cascade="save-update")
     wins = db.Column(db.Integer)
     losses = db.Column(db.Integer)
+
+    # 1 team to many events
+    #event_winner = db.relationship('Event', foreign_key=[Event.winner], backref='event_winner', lazy=True)
+    #event_loser = db.relationship('Event', foreign_key=[Event.loser], backref='event_loser', lazy=True)
 
     def __repr__(self):
         return '<Team: {}>'.format(self.name)
@@ -79,37 +111,21 @@ class Event(db.Model):
     __tablename__ = 'events'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    league_name = db.Column(db.String(60), db.ForeignKey('leagues.league_name'), nullable=False)
     day = db.Column(db.Date())
+
     winner = db.Column(db.String(200), db.ForeignKey('teams.name'), nullable=False)
     loser = db.Column(db.String(200), db.ForeignKey('teams.name'), nullable=False)
-    league_name = db.Column(db.String(60), db.ForeignKey('leagues.league_name'), nullable=False)
-    league_constraint = relationship("League", foreign_keys=[league_name])
-    winner_constraint = relationship("Team", foreign_keys=[winner])
-    loser_constraint = relationship("Team", foreign_keys=[loser])
+
+    # Must be in child table for foreign_keys to reference parent table
+    event_winner = db.relationship('Team', foreign_keys=[winner], backref='event_winner', lazy=True)
+    event_loser = db.relationship('Team', foreign_keys=[loser], backref='event_loser', lazy=True)
+
     winning_score = db.Column(db.Integer)
     losing_score = db.Column(db.Integer)
 
     def __repr__(self):
         return '<Event: {}>'.format(self.id)
-
-class League(db.Model):
-    """
-    Create an leagues table
-    """
-
-    __tablename__ = 'leagues'
-
-    league_name = db.Column(db.String(200), primary_key=True)
-    number_of_games = db.Column(db.Integer)
-    number_of_conferences = db.Column(db.Integer)
-    number_of_total_teams = db.Column(db.Integer)
-    number_of_rounds = db.Column(db.Integer)
-    number_of_qualifiers = db.Column(db.Integer)
-    is_byes = db.Column(db.String(200))
-
-
-    def __repr__(self):
-        return '<League: {}>'.format(self.name)
 
 class Ranking(db.Model):
     """
@@ -141,9 +157,9 @@ class Update(db.Model):
     username = db.Column(db.String(60), db.ForeignKey('users.username'), nullable=False)
     first_name = db.Column(db.String(60), index=True)
     last_name = db.Column(db.String(60), index=True)
-    username_constraint = relationship("User", foreign_keys=[username])
+    #username_constraint = relationship("User", foreign_keys=[username])
     league_name = db.Column(db.String(60), db.ForeignKey('leagues.league_name'), nullable=False)
-    league_constraint = relationship("League", foreign_keys=[league_name])
+    #league_constraint = relationship("League", foreign_keys=[league_name])
     phone_number = db.Column(db.String(10), index=True, unique=True)
     is_admin = db.Column(db.String(200))
 
