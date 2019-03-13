239bd0f5fa4f0c37daf6c469a897b695fdd0b7b1
Joshua Lemkin <joshua.lemkin@gmail.com>
2019-03-13 03:55:19 -0400

    Display user and admin leagues for when previously logged in user views home page before viewing other pages

--- app/home/views.py
+++ app/home/views.py
@@ -3,7 +3,8 @@
 from flask import render_template, session
 from flask_login import current_user, login_required
 from ..models import Team, Event, League, User, Registration
-
+from .helper import admin_and_user_leagues
+from .helperrankings import ranking_table
 from . import home
 
 @home.route('/')
@@ -11,7 +12,18 @@ def homepage():
     """
     Render the homepage template on the / route
     """
-    return render_template('home/index.html', title="Welcome")
+
+    # Display user's admin leagues and user leagues
+    if current_user.is_authenticated:
+        admin_leagues, user_leagues = admin_and_user_leagues(current_user.username)
+
+    # Set admin and user leagues to None if no user is logged in
+    else:
+        admin_leagues = None
+        user_leagues = None
+
+    return render_template('home/index.html', title="Welcome",
+                           admin_leagues=admin_leagues, user_leagues=user_leagues)
 
 @home.route('/dashboard')
 @login_required
@@ -20,28 +32,3 @@ def dashboard():
     Render the dashboard template on the /dashboard route
     """
     return render_template('home/dashboard.html', title="Dashboard")
-
-# @home.route('/<league_name>')
-# @login_required
-# def admin_dashboard(league_name):
-#
-#     def enough_teams(league_name):
-#
-#         def get_count(q):
-#             count_q = q.statement.with_only_columns([func.count()]).order_by(None)
-#             count_x = q.session.execute(count_q).scalar()
-#             return (count_x)
-#
-#         teamcount = get_count(Team.query.filter_by(league_name=league_name))
-#         team_requirement = League.query.filter_by(league_name=league_name).first().number_of_total_teams
-#
-#         if (teamcount == team_requirement):
-#             ranking_criteria = True
-#         else:
-#             ranking_criteria = False
-#
-#         return(ranking_criteria)
-#
-#     session['ranking_criteria'] = enough_teams(league_name)
-#
-#     return render_template('home/admin_dashboard.html', title=league_name, league_name=league_name)
